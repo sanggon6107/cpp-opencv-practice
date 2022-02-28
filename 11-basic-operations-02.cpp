91e96207076f23be33f4462b56c7c14d15a8bf6e
sanggon6107 <sanggon6107@naver.com>
2022-02-28 11:49:47 +0900

    basic operations-02

---
+++ 11-basic-operations-02.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+/* basic operations 02
+
+1. min, max
+min(A, B, dst); // dst = miin(A, B)
+max(A, 20, dst);
+
+2.
+exp(A, dst);
+log(A, dst);
+pow(A, 2.0, dst3);
+sqrt(A, dst);
+
+3.
+magnitutle(X, Y, mag);
+phase(X, Y, angle1);
+phase(X, Y, angle2, true);
+cartToPolar(X, Y, mag3, angle3);
+polarToCart(mag3, angle3, X3, Y3);
+
+4.
+int nCount = countNonZero(A);
+cout << sum(A) << endl;
+cout << mean(A) << endl;
+meanStdDev(A, avg, stddev); // 평균, 표준편차
+
+
+5. 그 외 방정식 풀기 등.
+*/
+
+
+int main()
+{
+
+}
