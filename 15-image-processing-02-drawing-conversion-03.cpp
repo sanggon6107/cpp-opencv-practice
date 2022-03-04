f000758893931aafbe4f79429807f1bc8f2ec1d6
sanggon6107 <sanggon6107@naver.com>
2022-03-04 19:20:01 +0900

    직선반복자

--- 15-image-processing-02-drawing-conversion-03.cpp
+++ 15-image-processing-02-drawing-conversion-03.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <type_traits>
 
 using namespace std;
 using namespace cv;
@@ -22,11 +23,17 @@ int main()
 	Point pt_1(10, 10), pt_2(400, 400);
 	LineIterator itr(img, pt_1, pt_2, 8);
 	
-	vector<Vec3b> pts(itr.count);
+	cout << "itr.count : " << itr.count << endl // 멤버 변수 count는 직선에 포함된 좌표의 개수이다.
+		<< "itr.pos() : " << itr.pos() << endl; // 멤버 함수 pos()는 현재 이터레이터가 가리키고 있는 좌표를 반환한다.
+	
+	cout << "type of itr.pos() : " << typeid(itr.pos()).name() << endl; // type of itr.pos() : class cv::Point_<int>
+
+
+	vector<Vec3b> pts(itr.count); // itr 개수만큼 Vec3b 객체를 가지는 벡터 선언.
 	for (int i = 0; i < itr.count; i++, ++itr)
 	{
 		auto pos = itr.pos();
-		pts[i] = img.at<Vec3b>(pos);
+		pts[i] = img.at<Vec3b>(pos); // pos위치의 Vec3b를 pts에 넣는다.
 	}
 	line(img, pt_1, pt_2, Scalar(255, 255, 255)); // 어떤 곳 그엇는지 표시하기.
 
